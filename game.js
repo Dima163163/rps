780b24772da049138a69c46f34ca04375b3f71b4
Dima163163 <khlopovdima@gmail.com>
2023-09-20 20:55:36 +0400

    fix loalwords

--- game.js
+++ game.js
@@ -17,28 +17,22 @@
       computer: 0,
     };
     const language = prompt('Выберите язык?', 'EN');
+    const localeWords = {
+      'en': ['rock', 'scissors', 'paper',
+        'Game Over', 'User point',
+        'Computer point', 'Computer', 'User', 'Draw!',
+        'You victory!', 'You lost!'],
+      'ru': ['камень', 'ножницы', 'бумага',
+        'Игра окончена', 'Очков у пользователя',
+        'Очков у компьютера', 'Компьютер', 'Вы',
+        'Ничья!', 'Вы победили!', 'Вы проиграли!'],
+    };
     const lang = language === 'EN' || language === 'ENG' ?
-      FIGURES_ENG : FIGURES_RUS;
-    const gameOver = language === 'EN' || language === 'ENG' ?
-    'Game Over' : 'Игра окончена';
-    const userPoint = language === 'EN' || language === 'ENG' ?
-    'User point' : 'Очков у пользователя';
-    const computerPoint = language === 'EN' || language === 'ENG' ?
-    'Computer point' : 'Очков у компьютера';
-    const computer = language === 'EN' || language === 'ENG' ?
-    'Computer' : 'Компьютер';
-    const user = language === 'EN' || language === 'ENG' ?
-    'User' : 'Вы';
-    const draw = language === 'EN' || language === 'ENG' ?
-    'Draw!' : 'Ничья!';
-    const victory = language === 'EN' || language === 'ENG' ?
-    'You victory!' : 'Вы победили!';
-    const defeat = language === 'EN' || language === 'ENG' ?
-    'You lost!' : 'Вы проиграли!';
+      localeWords['en'] : localeWords['ru'];
     return function start() {
       let userFigure;
       let action;
-      if (lang === FIGURES_ENG) {
+      if (lang === localeWords['en']) {
         userFigure = prompt(`rock, scissors, paper`, '');
       } else {
         userFigure = prompt(`камень, ножницы, бумага`, '');
@@ -54,35 +48,35 @@
         ) {
           ++result.player;
           alert(`
-            ${computer}: ${computerFigure} \n
-            ${user}: ${userFigure.toLowerCase()} \n 
-            ${victory}`);
+            ${lang[6]}: ${computerFigure} \n
+            ${lang[7]}: ${userFigure.toLowerCase()} \n 
+            ${lang[9]}`);
           start();
         } else if (userFigure.toLowerCase()[0] === computerFigure[0]) {
           alert(`
-            ${computer}: ${computerFigure} \n
-            ${user}: ${userFigure.toLowerCase()} \n
-            ${draw}`);
+            ${lang[6]}: ${computerFigure} \n
+            ${lang[7]}: ${userFigure.toLowerCase()} \n
+            ${lang[8]}`);
           start();
         } else {
           ++result.computer;
           alert(`
-            ${computer}: ${computerFigure} \n
-            ${user}: ${userFigure.toLowerCase()} \n
-            ${defeat}`);
+            ${lang[6]}: ${computerFigure} \n
+            ${lang[7]}: ${userFigure.toLowerCase()} \n
+            ${lang[10]}`);
           start();
         }
       } else {
-        if (lang === FIGURES_ENG) {
+        if (lang === localeWords['en']) {
           action = confirm('Are you sure you want to go out?');
         } else {
           action = confirm('Точно ли вы хотите выйти?');
         }
         if (action) {
           alert(`
-          ${gameOver} \n
-          ${userPoint}: ${result.player} \n
-          ${computerPoint}: ${result.computer}
+          ${lang[3]} \n
+          ${lang[4]}: ${result.player} \n
+          ${lang[5]}: ${result.computer}
           `);
           return;
         }
